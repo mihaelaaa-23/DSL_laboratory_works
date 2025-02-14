2e0025461a3404a2430c6e9bf7a577c323447db2
mihaelaaa-23 <mihaela.untu@gmail.com>
2025-02-14 15:04:33 +0200

    Updated Grammar Class to implement finiteAutomaton function

--- 1_RegularGrammars/grammar.py
+++ 1_RegularGrammars/grammar.py
@@ -1,4 +1,5 @@
 import random
+from finite_automaton import FiniteAutomaton
 class Grammar:
     def __init__(self):
         self.vn = {'S', 'A', 'B', 'C'}
@@ -20,3 +21,23 @@ class Grammar:
                     break
         return string
 
+    def to_finite_automaton(self):
+        q = self.vn  # states
+        sigma = self.vt  # alphabet
+        q0 = self.start_symbol  # start state
+        delta = {}  # Transition func
+
+        for key in self.p.keys():
+            for rule in self.p[key]:
+                if key not in delta:
+                    delta[key] = []
+                if len(rule) == 2:
+                    delta[key].append((rule[0], rule[1]))  # (input symbol, next state)
+                else:
+                    delta[key].append((rule[0], None))  # terminal transition
+
+        # find final states (terminal only strings)
+        f = {key for key in self.p if all(rule in self.vt for rule in self.p[key])}
+        f.add('C')  # C is final state
+
+        return FiniteAutomaton(q, sigma, delta, q0, f)
