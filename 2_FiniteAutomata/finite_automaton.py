bde67017b9ca69681a2e9e5487f7e2685828d1f2
mihaelaaa-23 <mihaela.untu@gmail.com>
2025-02-23 18:31:27 +0200

    Implemented functionality that converts NFA to DFA

--- 2_FiniteAutomata/finite_automaton.py
+++ 2_FiniteAutomata/finite_automaton.py
@@ -35,4 +35,33 @@ class FiniteAutomaton:
                 seen_symbols.add(symbol)
         return True
 
+    def convert_ndfa_to_dfa(self):
+        dfa_states = {}
+        queue = []
+        start_state = frozenset([self.q0])
+
+        dfa_states[start_state] = "q0"
+        queue.append(start_state)
+        dfa_transitions = {}
+
+        while queue:
+            current_set = queue.pop(0)
+            dfa_state_name = dfa_states[current_set]
+
+            for symbol in self.sigma:
+                new_set = set()
+                for state in current_set:
+                    if state in self.delta and symbol in self.delta[state]:
+                        new_set.update(self.delta[state][symbol])
+
+                if new_set:
+                    frozen_new_set = frozenset(new_set)
+                    if frozen_new_set not in dfa_states:
+                        dfa_states[frozen_new_set] = f"q{len(dfa_states)}"
+                        queue.append(frozen_new_set)
+
+                    dfa_transitions[dfa_state_name, symbol] = dfa_states[frozen_new_set]
+
+        return dfa_transitions, dfa_states
+
 
