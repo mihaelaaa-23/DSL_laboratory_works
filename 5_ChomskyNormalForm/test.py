a6dc9f77b2f5af5cf7c7ff4c77314f64a6504721
mihaelaaa-23 <mihaela.untu@gmail.com>
2025-04-06 21:18:52 +0300

    unitTests to check conversion works well

---
+++ 5_ChomskyNormalForm/test.py
@@ -0,0 +1,77 @@
+import unittest
+
+from grammar import Grammar
+
+
+class TestGrammar(unittest.TestCase):
+    def setUp(self):
+        # variant 29 grammar
+        non_terminals = ['S', 'A', 'B', 'C', 'D', 'X']
+        terminals = ['a', 'b']
+        rules = {
+            'S': ['B'],
+            'A': ['aX', 'bX'],
+            'X': ['BX', 'b', 'ε'],
+            'B': ['AXaD'],
+            'D': ['a', 'aD'],
+            'C': ['Ca']
+        }
+        self.grammar = Grammar(non_terminals, terminals, rules)
+
+    def test_initial_grammar(self):
+        self.assertEqual(self.grammar.non_terminals, ['S', 'A', 'B', 'C', 'D','X'])
+        self.assertEqual(self.grammar.terminals, ['a', 'b'])
+        self.assertIn('S', self.grammar.rules)
+        self.assertIn('A', self.grammar.rules)
+        self.assertIn('B', self.grammar.rules)
+        self.assertIn('C', self.grammar.rules)
+        self.assertIn('D', self.grammar.rules)
+
+    def test_eliminate_epsilon_productions(self):
+        self.grammar.eliminate_epsilon_productions()
+        self.assertNotIn('ε', self.grammar.rules['D'])
+
+    def test_eliminate_renaming_productions(self):
+        self.grammar.eliminate_epsilon_productions()
+        self.grammar.eliminate_renaming()
+        for prods in self.grammar.rules.values():
+            for prod in prods:
+                self.assertNotIn(prod, self.grammar.non_terminals)
+
+    def test_eliminate_inaccessible_symbols(self):
+        self.grammar.eliminate_epsilon_productions()
+        self.grammar.eliminate_renaming()
+        self.grammar.eliminate_inaccessible_symbols()
+        for nt in self.grammar.non_terminals:
+            self.assertIn(nt, self.grammar.rules)
+
+    def test_eliminate_non_productive_symbols(self):
+        self.grammar.eliminate_epsilon_productions()
+        self.grammar.eliminate_renaming()
+        self.grammar.eliminate_inaccessible_symbols()
+        self.grammar.eliminate_non_productive_symbols()
+        for prods in self.grammar.rules.values():
+            for prod in prods:
+                self.assertTrue(all(
+                    symbol in self.grammar.terminals or symbol in self.grammar.non_terminals for symbol in prod))
+
+    def test_is_cnf(self):
+        self.assertFalse(self.grammar.is_cnf())
+        self.grammar.to_cnf(print_steps=False)
+        self.assertTrue(self.grammar.is_cnf())
+
+    def test_to_cnf(self):
+        self.grammar.to_cnf(print_steps=True)
+        for nt, prods in self.grammar.rules.items():
+            for prod in prods:
+                self.assertTrue(len(prod) <= 2)
+                if len(prod) == 2:
+                    self.assertTrue(
+                        all(symbol in self.grammar.non_terminals for symbol in prod))
+                if len(prod) == 1:
+                    self.assertTrue(
+                        prod in self.grammar.terminals or prod == 'ε')
+
+
+if __name__ == '__main__':
+    unittest.main()
